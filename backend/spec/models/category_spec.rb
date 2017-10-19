ba47f65ad728ce3e48bffe2558982c073f7236b4
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-10-19 13:03:19 +0300

    add test for catagory

--- backend/spec/models/category_spec.rb
+++ backend/spec/models/category_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do 
+    @category = create(:category)
+  end
+
+  subject { @category }
+
+  describe "Category parameters" do
+
+   it { should respond_to :name } 
+   it { should respond_to :description }
+
+  end
+
+  describe "Validation create category" do
+
+    describe "Cannot create categoty if" do
+
+      describe "Name start with lowercase character" do
+        @category.name = "british humor."
+        it { should_not be_valid }
+      end
+      
+      describe "Name have one word" do
+        @category.name = "Humor."
+        it { should_not be_valid }
+      end
+      
+      describe "The first word of a name have less than two laters" do
+        @category.name = "B humor."
+        it { should_not be_valid }
+      end
+
+      describe "The second word of a name have less than two laters" do
+        @category.name = "British h."
+        it { should_not be_valid }
+      end
+
+      describe "Name doesn't end with point" do
+        @category.name = "British humor"
+        it { should_not be_valid }
+      end
+    end
+  end
 end
