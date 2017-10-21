bc804b8263ad2ac9cc7bdeb968efa7d8d5e598e0
Rubyist007 <VasiliyDyblinchyk@gmail.com>
2017-10-21 14:26:26 +0300

    add model with validation and has success test

--- backend/app/helpers/application_helper.rb
+++ backend/app/helpers/application_helper.rb
@@ -1,2 +1,3 @@
 module ApplicationHelper
+  REGEX_COMMON_VALID_PROPERTIES_NAME = /\A([A-Z][^\s]+) ([^\s]+{2,}) *.+[.]\z/
 end
--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  validates_presence_of :name
+  validates :name, format: { with: REGEX_COMMON_VALID_PROPERTIES_NAME }
 end
