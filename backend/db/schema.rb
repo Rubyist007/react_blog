e052b8b852b5de356da00e07c676e2c2aef74d6a
Your Name <you@example.com>
2017-10-22 21:54:11 +0100

    add migrate for comment and run it

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022172137) do
+ActiveRecord::Schema.define(version: 20171022191928) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
