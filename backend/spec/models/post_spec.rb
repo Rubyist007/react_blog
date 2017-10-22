30983955d5256d2783f48be6167165e4dbb5eb9a
Your Name <you@example.com>
2017-10-22 20:12:21 +0100

    add test on size file

--- backend/spec/models/post_spec.rb
+++ backend/spec/models/post_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Post, type: :model do
   describe "Post parameters" do
 
    it { should respond_to :name } 
-   it { should respond_to :description }
+   it { should respond_to :content }
    it { should respond_to :file }
 
   end
@@ -21,37 +21,38 @@ RSpec.describe Post, type: :model do
     describe "Cannot create post if" do
         
       describe "Post not have name" do
-        before { @category.name = nil }
+        before { @post.name = nil }
         it { should_not be_valid }
       end
 
       describe "Name start with lowercase character" do
-        before { @category.name = "british humor." }
+        before { @post.name = "british humor." }
         it { should_not be_valid }
       end
       
       describe "Name have one word" do
-        before { @category.name = "Humor." }
+        before { @post.name = "Humor." }
         it { should_not be_valid }
       end
       
       describe "The first word of a name have less than two laters" do
-        before { @category.name = "B humor." }
+        before { @post.name = "B humor." }
         it { should_not be_valid }
       end
 
       describe "The second word of a name have less than two laters" do
-        before { @category.name = "British h." }
+        before { @post.name = "British h." }
         it { should_not be_valid }
       end
 
       describe "Name doesn't end with point" do
-        before { @category.name = "British humor" }
+        before { @post.name = "British humor" }
         it { should_not be_valid }
       end
 
       describe "File weigh bigger than 2mb." do 
-
+        before { @post.file = File.open("#{Rails.root}/app/assets/images/test_image/NASA_Image.jpg") }
+        it { should_not be_valid }
       end
     end
   end
